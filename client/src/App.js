9b42ac379306be68dfb91aaa4a0b01faf73255a5
oliverpople <oliverpople@gmail.com>
2019-04-12 15:35:29 -0700

    Switched to video-react html5 player as the video customisation settings are superior.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import movie from "./movie.mp4";
 import subtitles from "./subtitles.vtt";
-import { DefaultPlayer as Video } from "react-html5video";
-import "react-html5video/dist/styles.css";
+import { Player, ControlBar } from "video-react";
+import "video-react/dist/video-react.css";
 
-export default function App() {
-  return (
-    <Video
-      autoPlay
-      loop
-      muted
-      controls={["PlayPause", "Seek", "Time", "Volume", "Fullscreen"]}
-      poster=""
-      onCanPlayThrough={() => {
-        // Do stuff
-      }}
-    >
-      <source src={movie} type="video/mp4" />
-      <track
-        label="English"
-        kind="subtitles"
-        srcLang="en"
-        src={subtitles}
-        default
-      />
-    </Video>
-  );
+export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      playerSource: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Player
+          fluid={false}
+          width={300}
+          ref="player"
+          videoId="video-1"
+          muted={true}
+          autoPlay={true}
+          loop
+        >
+          <source src={this.state.playerSource} />
+          <ControlBar autoHide={true} />
+          <track
+            kind="captions"
+            srcLang="en-US"
+            label="English"
+            default
+            src={subtitles}
+          />
+        </Player>
+      </div>
+    );
+  }
 }
