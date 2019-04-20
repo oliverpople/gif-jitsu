9d6d3f72fd3c69d1d0c30471dc77a9fa1dfd1922
oliverpople <oliverpople@gmail.com>
2019-04-19 17:16:29 -0700

    Removed API route as function (vtt parsing) is now handled in front end.

--- server/index.js
+++ server/index.js
@@ -17,21 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("*", function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 app.get("/", (req, res) => res.send("Hello World!"));
-
-app.use("/vttparser", vttparser);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
