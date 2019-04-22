377e56e920b1d61118e08b92d687838eb9719989
oliverpople <oliverpople@gmail.com>
2019-04-22 12:01:16 -0700

    Subtitle form now handles form data and hands back to parent via props handler.

--- client/src/App.js
+++ client/src/App.js
@@ -2,53 +2,34 @@ import React, { Component } from "react";
 import movie from "./movie.mp4";
 import { compile } from "node-webvtt";
 import VideoPlayer from "./VideoPlayer";
+import SubtitleForm from "./SubtitleForm";
 
 export default class App extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setInputJson = this.setInputJson.bind(this);
     this.renderVideoWithSubInputs = this.renderVideoWithSubInputs.bind(this);
     this.compileJsonSubtitleInput = this.compileJsonSubtitleInput.bind(this);
 
     this.state = {
       playerSource: movie,
-      subs: "",
-      subtitleText: "",
-      startTime: "",
-      endTime: "",
-      inputJson: {},
-      submitted: false
+      compiledSubs: "",
+      inputJson: {}
     };
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit = async event => {
-    event.preventDefault();
-    const inputJson = {
-      valid: true,
-      cues: [
-        {
-          identifier: "",
-          start: this.state.startTime,
-          end: this.state.endTime,
-          text: this.state.subtitleText,
-          styles: ""
-        }
-      ]
-    };
+  async setInputJson(inputJson) {
     await this.setState({ inputJson });
     await this.compileJsonSubtitleInput();
     this.setState({ submitted: true });
-  };
+  }
 
   renderVideoWithSubInputs() {
-    if (this.state.submitted) {
-      return <VideoPlayer playerSource={movie} subs={this.state.subs} />;
+    if (this.state.inputJson.hasOwnProperty("valid")) {
+      return (
+        <VideoPlayer playerSource={movie} subs={this.state.compiledSubs} />
+      );
     }
   }
 
@@ -56,43 +37,14 @@ export default class App extends Component {
     const subtitleText = compile(this.state.inputJson);
     const file = new Blob([subtitleText], { type: "html/txt" });
     const fileURL = URL.createObjectURL(file);
-    this.setState({ subs: fileURL });
+    this.setState({ compiledSubs: fileURL });
   }
 
   render() {
     return (
       <div>
         {this.renderVideoWithSubInputs()}
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Subtitle text:
-            <input
-              name="subtitleText"
-              type="text"
-              value={this.state.subtitleText}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Start time:
-            <input
-              name="startTime"
-              type="text"
-              value={this.state.startTime}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            End time:
-            <input
-              name="endTime"
-              type="text"
-              value={this.state.endTime}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <SubtitleForm setInputJson={this.setInputJson} />
       </div>
     );
   }
---
+++ client/src/SubtitleForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+export default class SubtitleForm extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      inputJson: {
+        valid: true,
+        cues: [
+          {
+            identifier: "",
+            start: "",
+            end: "",
+            text: "",
+            styles: ""
+          }
+        ]
+      }
+    };
+  }
+
+  handleChange(event) {
+    var nestedStateProperty = { ...this.state.inputJson };
+    nestedStateProperty.cues[0][event.target.name] = event.target.value;
+    this.setState({ inputJson: nestedStateProperty });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.props.setInputJson(this.state.inputJson);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Subtitle text:
+            <input
+              name="text"
+              type="text"
+              value={this.state.inputJson.cues[0].text}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Start time:
+            <input
+              name="start"
+              type="text"
+              value={this.state.inputJson.cues[0].start}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            End time:
+            <input
+              name="end"
+              type="end"
+              value={this.state.inputJson.cues[0].end}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
