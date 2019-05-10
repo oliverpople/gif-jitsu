ed092e927b20f9558131fdca2a998ca84378cb3f
oliverpople <oliverpople@gmail.com>
2019-05-10 12:06:31 -0700

    Refactor.

--- server/routes/mongodb.js
+++ server/routes/mongodb.js
@@ -7,14 +7,14 @@ const ObjectID = require("mongodb").ObjectID;
 var express = require("express");
 const router = express.Router();
 
+var uri = process.env.DB_ROUTE;
+
 router.post("/convertURLToMP4", (req, res) => {
   var videoMp4Stream = ytdl(req.body.YTUrl).pipe(
     fs.createWriteStream("convertedVideo.mp4")
   );
 
   videoMp4Stream.on("finish", function() {
-    var uri = process.env.DB_ROUTE;
-
     mongodb.MongoClient.connect(
       uri,
       { useNewUrlParser: true },
@@ -41,38 +41,7 @@ router.post("/convertURLToMP4", (req, res) => {
   });
 });
 
-router.post("/getUrlStreamForVideoWithId", (req, res) => {
-  var id = req.body.id;
-  var uri = process.env.DB_ROUTE;
-
-  mongodb.MongoClient.connect(
-    uri,
-    { useNewUrlParser: true },
-    function(error, db) {
-      assert.ifError(error);
-
-      var bucket = new mongodb.GridFSBucket(db, { bucketName: "videos" });
-
-      var downloadStream = bucket.openDownloadStream(new ObjectID(id));
-      var writeStream = fs.createWriteStream(id + ".mp4");
-
-      downloadStream
-        .pipe(writeStream)
-        .on("error", function(error) {
-          assert.ifError(error);
-        })
-        .on("finish", function() {
-          console.log("Downloaded video to Server!");
-          fs.createReadStream(id + ".mp4").pipe(res);
-          fs.unlink(id + ".mp4", function(err) {});
-        });
-    }
-  );
-});
-
 router.get("/getAllVideoFileIds", (req, res) => {
-  var uri = process.env.DB_ROUTE;
-
   mongodb.MongoClient.connect(
     uri,
     { useNewUrlParser: true },
@@ -88,6 +57,33 @@ router.get("/getAllVideoFileIds", (req, res) => {
           if (err) throw err;
           res.json({ fileIdsArray });
           db.close();
+        });
+    }
+  );
+});
+
+router.post("/getUrlStreamForVideoWithId", (req, res) => {
+  var id = req.body.id;
+
+  mongodb.MongoClient.connect(
+    uri,
+    { useNewUrlParser: true },
+    function(error, db) {
+      assert.ifError(error);
+
+      var bucket = new mongodb.GridFSBucket(db, { bucketName: "videos" });
+      var downloadStream = bucket.openDownloadStream(new ObjectID(id));
+      var writeStream = fs.createWriteStream(id + ".mp4");
+
+      downloadStream
+        .pipe(writeStream)
+        .on("error", function(error) {
+          assert.ifError(error);
+        })
+        .on("finish", function() {
+          console.log("Downloaded video to Server!");
+          fs.createReadStream(id + ".mp4").pipe(res);
+          fs.unlink(id + ".mp4", function(err) {});
         });
     }
   );
