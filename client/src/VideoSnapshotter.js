84d0eb9c90bd6fd6563912f8c3ab3033222682ff
oliverpople <oliverpople@gmail.com>
2019-06-04 13:08:48 -0700

    Videosnapshotter now returns images instead of canvas snapshots, but cors only works becuase of bypassing chrome extension.

--- client/src/VideoSnapshotter.js
+++ client/src/VideoSnapshotter.js
@@ -21,43 +21,41 @@ export default class VideoSnapper extends Component {
     });
   }
 
-  capture = video => {
-    let { scaleFactor } = this.state;
+  capture = () => {
+    let { videoRef, outputRef, scaleFactor } = this.state;
     if (scaleFactor == null) {
       scaleFactor = 1;
     }
+    var output = outputRef;
+    var video = videoRef;
+
     var w = video.videoWidth * scaleFactor;
     var h = video.videoHeight * scaleFactor;
+
     var canvas = document.createElement("canvas");
     canvas.width = w;
     canvas.height = h;
     var ctx = canvas.getContext("2d");
     ctx.drawImage(video, 0, 0, w, h);
-    return canvas;
-  };
-
-  shoot = () => {
-    let { snapshots, videoRef, outputRef } = this.state;
-    if (videoRef && outputRef) {
-      var video = videoRef;
-      console.log(video);
-      var output = outputRef;
-      var canvas = this.capture(video);
-      snapshots.unshift(canvas);
-      console.log(snapshots);
-      output.innerHTML = "";
-      this.state.snapshots.map(snapshot => output.appendChild(snapshot));
-    }
+    var image = new Image(w, h);
+    image.crossOrigin = "anonymous";
+    image.src = canvas.toDataURL();
+    output.prepend(image);
   };
 
   render() {
     return (
       <div className="wrap">
-        <video ref={this.videoRef} width={320} controls={true}>
+        <video
+          crossOrigin="anonymous"
+          ref={this.videoRef}
+          width={320}
+          controls={true}
+        >
           <source src="http://techslides.com/demos/sample-videos/small.webm" />
         </video>
         <br />
-        <button id="cit" onClick={this.shoot} className="button">
+        <button id="cit" onClick={this.capture} className="button">
           Capture
         </button>
         <br />
